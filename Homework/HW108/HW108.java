c62b5ec484a9d3f18a5d61cfc02e2859dcf52fa1
Emily Greene <798007@seq.org>
2020-01-09 22:36:14 -0800

    109

---
+++ Homework/HW108/HW108.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+/**
+ * Homework 1/8/20
+ *
+ * @author (Emily Greene)
+ * @version (109)
+ */
+public class HW108
+{
+    //constructor
+    public HW108()
+    {
+    }
+    //method that takes in an arraylist of strings and reverses the order
+    public ArrayList<String> reverseList(ArrayList<String> list){
+        ArrayList<String> newList = new ArrayList<String>();
+        for(int i = 0; i < list.size(); i++){
+            newList.add(list.get(list.size()-1-i));
+        }
+        return newList;
+    }
+    //method that takes in an array of strings and reverses the order
+    public String[] reverseArr(String[] list){
+        String[] newList = new String[list.length];
+        for(int i = 0; i < list.length; i++){
+            newList[i] = list[list.length - 1 - i];
+        }
+        return newList;
+    }
+}
