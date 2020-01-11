51cffcce9fc36bd4e312e589d169c49ebca794c4
Emily Greene <798007@seq.org>
2020-01-10 22:04:54 -0800

    110(2)

--- Homework/HW108/Reverse.java
+++ Homework/HW108/Reverse.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 /**
- * Homework 1/8/20
+ * Homework 1/8/20: Reversing Arrays and ArrayLists
  *
  * @author (Emily Greene)
  * @version (109)
@@ -8,10 +8,21 @@ import java.util.ArrayList;
 public class Reverse
 {
     //instance variables
-    
+    ArrayList<String> sl = new ArrayList<String>();
+    String[] sa = {"Bob", "James", "Will", "Ryan", "John"};
     //constructor
-    public Reverse()
-    {
+    public Reverse(){
+        sl.add("Bob");
+        sl.add("James");
+        sl.add("Will");
+        sl.add("Ryan");
+        sl.add("John");
+        reverseList(sl);
+        reverseArr(sa);
+    }
+    //main method
+    public static void main(){
+        Reverse run = new Reverse();
     }
     //method that takes in an arraylist of strings and reverses the order
     public ArrayList<String> reverseList(ArrayList<String> list){
@@ -19,6 +30,8 @@ public class Reverse
         for(int i = 0; i < list.size(); i++){
             newList.add(list.get(list.size()-1-i));
         }
+        System.out.println("Original arraylist: " +  list);
+        System.out.println("Reversed arraylist: " + newList);
         return newList;
     }
     //method that takes in an array of strings and reverses the order
@@ -26,6 +39,14 @@ public class Reverse
         String[] newList = new String[list.length];
         for(int i = 0; i < list.length; i++){
             newList[i] = list[list.length - 1 - i];
+        }
+        System.out.print("Original array: ");
+        for(int i = 0; i < newList.length; i++){
+            System.out.print(list[i] + ", ");
+        }
+        System.out.print("\nReversed array: ");
+        for(int i = 0; i < newList.length; i++){
+            System.out.print(newList[i] + ", ");
         }
         return newList;
     }
