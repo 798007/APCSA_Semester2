d3079a20e28deb9622176f2398e2bcbc7f2300e6
Emily Greene <798007@seq.org>
2020-01-13 22:04:18 -0800

    113

--- Labs/InterfacesLab/WordRunner.java
+++ Labs/InterfacesLab/WordRunner.java
@@ -3,18 +3,29 @@
 
 //comparable example one
 
-import static java.lang.System.*;
+import java.util.*;
 
 public class WordRunner
 {
-	public static void main ( String[] args )
-	{
-		Word x = new Word("dog");
-		Word y = new Word("cat");
-		System.out.println( x.compareTo(y) );
-		
-		//make a list of Word
-		//call Collections.sort() and sort the list
-		//print the list
+    public static void main ( String[] args )
+    {
+        Word x = new Word("dog");
+        Word y = new Word("cat");
+        System.out.println( x.compareTo(y) );
+        
+        //make a list of Word
+        //call Collections.sort() and sort the list
+        //print the list
+        
+        Word[] wl = new Word[5];
+        wl[0] = new Word("hamster");
+        wl[1] = new Word("dolphin");
+        wl[2] = new Word("puppy");
+        wl[3] = new Word("elephant");
+        wl[4] = new Word("monkey");
+        Arrays.sort(wl);
+        for(int i = 0; i < wl.length; i++){
+          System.out.print(wl[i] + ", ");
+        }
   }
 }
