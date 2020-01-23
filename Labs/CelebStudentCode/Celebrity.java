ab141cab5615f3d00fb197d2fa14a2398ea8a91e
Emily Greene <798007@seq.org>
2020-01-22 23:49:11 -0800

    122

--- Labs/CelebStudentCode/Celebrity.java
+++ Labs/CelebStudentCode/Celebrity.java
@@ -5,66 +5,70 @@
  */
 public class Celebrity
 {
-	/**
-	 * The clue to determine the celebrity
-	 */
-	
-	/**
-	 * The answer or name of the celebrity.
-	 */
-	
-	/**
-	 * Creates a Celebrity instance with the supplied answer and clue
-	 * @param answer
-	 * @param clue
-	 */
-	public Celebrity(String answer, String clue)
-	{
-	}
+    /**
+     * The clue to determine the celebrity
+     */
+    private String clue = "";
+    /**
+     * The answer or name of the celebrity.
+     */
+    private String celeb = "";
+    
+    /**
+     * Creates a Celebrity instance with the supplied answer and clue
+     * @param answer
+     * @param clue
+     */
+    public Celebrity(String answer, String clue)
+    {
+        this.clue = clue;
+        celeb = answer;
+        Celebrity c = new Celebrity(celeb, this.clue);
+    }
 
-	/**
-	 * Supplies the clue for the celebrity
-	 * @return
-	 */
-	public String getClue()
-	{
-		return null;
-	}
+    /**
+     * Supplies the clue for the celebrity
+     * @return
+     */
+    public String getClue()
+    {
+        return clue;
+    }
 
-	/**
-	 * Supplies the answer for the celebrity.
-	 * @return
-	 */
-	public String getAnswer()
-	{
-		return null;
-	}
+    /**
+     * Supplies the answer for the celebrity.
+     * @return
+     */
+    public String getAnswer()
+    {
+        return celeb;
+    }
 
-	/**
-	 * Updates the clue to the provided String.
-	 * @param clue The new clue.
-	 */
-	public void setClue(String clue)
-	{
-		
-	}
+    /**
+     * Updates the clue to the provided String.
+     * @param clue The new clue.
+     */
+    public void setClue(String clue)
+    {
+       this.clue = clue; 
+    }
 
-	/**
-	 * Updates the answer to the provided String.
-	 * @param answer The new answer.
-	 */
-	public void setAnswer(String answer)
-	{
-		
-	}
-	
-	/**
-	 * Provides a String representation of the Celebrity.
-	 */
-	@Override
-	public String toString()
-	{
-		return null;
-	}
-	
+    /**
+     * Updates the answer to the provided String.
+     * @param answer The new answer.
+     */
+    public void setAnswer(String answer)
+    {
+        celeb = answer;
+    }
+    
+    /**
+     * Provides a String representation of the Celebrity.
+     */
+    @Override
+    public String toString()
+    {
+        return null;
+    }
+    
 }
