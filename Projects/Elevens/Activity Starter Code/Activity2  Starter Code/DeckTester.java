2b8e398b83e61c7888412d12a0bbfbe25ebe1701
Emily Geene <egreene@WS-ACADBUS.EDU>
2020-01-31 10:25:20 -0800

    131

---
+++ Projects/Elevens/Activity Starter Code/Activity2  Starter Code/DeckTester.java
@@ -0,0 +1,16 @@
+/**
+ * This is a class that tests the Deck class.
+ */
+public class DeckTester {
+
+	/**
+	 * The main method in this class checks the Deck operations for consistency.
+	 *	@param args is not used.
+	 */
+	public static void main(String[] args) {
+		String[] ranks = {"jack", "queen", "king"};
+		String[] suits = {"black",  "red"};
+		int[] pointValues = {11, 12, 13};
+		Deck d = new Deck(ranks, suits, pointValues);
+	}
+}
